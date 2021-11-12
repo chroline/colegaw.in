2158d90d2cb903f03056c92782824b52a4a67029
Cole Gawin <colegawin@gmail.com>
2021-11-11 23:38:31 -0600

    ✨ add SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { NextSeo } from "next-seo";
+import Head from "next/head";
+
 import { BrandgineerInfo } from "~/components/brandgineer-info";
 import { Footer } from "~/components/footer";
 import { Hero } from "~/components/hero";
@@ -9,6 +12,26 @@ import { Section } from "~/components/shared/Section";
 const Index = () => {
   return (
     <>
+      <Head>
+        <title>Cole Gawin</title>
+      </Head>
+      <NextSeo
+        title="Cole Gawin"
+        description="Working with passionate brands is my passion. I work with people to deliver quality experiences to meaningful audiences."
+        openGraph={{
+          url: "https://colegaw.in",
+          title: "Cole Gawin",
+          description:
+            "Working with passionate brands is my passion. I work with people to deliver quality experiences to meaningful audiences.",
+          images: [{ url: "https://colegaw.in/img/seo.png" }],
+          site_name: "Cole Gawin",
+        }}
+        twitter={{
+          handle: "@colegawin_",
+          site: "@colegawin_",
+          cardType: "summary_large_image",
+        }}
+      />
       <Section>
         <Hero />
       </Section>
