a7cc7937988a2be5334afa01edca2a7795ae7180
Cole Gawin <colegawin@gmail.com>
2021-11-19 00:48:07 -0600

    ✨ update navbar button styles

--- src/components/navbar/Buttons.tsx
+++ src/components/navbar/Buttons.tsx
@@ -7,16 +7,34 @@ export const NavbarButtons = () => (
   <HStack>
     <Button
       as={LinkBox}
-      colorScheme={"teal"}
-      shadow={"xs"}
+      background={"gray.50"}
+      border={"1px solid"}
+      borderColor={"gray.200"}
+      color={"blue.600"}
+      _hover={{
+        borderColor: "gray.300",
+        background: "gray.100",
+      }}
+      _active={{ borderColor: "gray.200" }}
       rightIcon={<HiExternalLink />}
-      display={{ base: "none", lg: "flex" }}
     >
       <LinkOverlay href={"https://projects.colegaw.in"} isExternal>
         Portfolio
       </LinkOverlay>
     </Button>
-    <Button as={LinkBox} colorScheme={"amber"} shadow={"xs"} rightIcon={<HiExternalLink />}>
+    <Button
+      as={LinkBox}
+      background={"gray.50"}
+      border={"1px solid"}
+      borderColor={"gray.200"}
+      color={"amber.600"}
+      _hover={{
+        borderColor: "gray.300",
+        background: "gray.100",
+      }}
+      _active={{ borderColor: "gray.200" }}
+      rightIcon={<HiExternalLink />}
+    >
       <LinkOverlay href={"https://blog.colegaw.in"} isExternal>
         Read my blog
       </LinkOverlay>
