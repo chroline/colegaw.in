c5a03937bbd4c7ea4b16b517fae1881382f8aba6
Cole Gawin <colegawin@gmail.com>
2021-12-19 17:42:40 -0600

    🚀 replace socials with link to lynk.colegaw.in

--- src/components/shared/Socials.tsx
+++ src/components/shared/Socials.tsx
@@ -1,71 +1,18 @@
 import React from "react";
 
-import { HStack, Link, Stack, Tooltip } from "@chakra-ui/react";
-import {
-  RiAtFill,
-  RiGithubFill,
-  RiGitlabFill,
-  RiInstagramFill,
-  RiLinkedinBoxFill,
-  RiTwitterFill,
-} from "react-icons/ri";
-import { SiBuymeacoffee } from "react-icons/si";
+import { Box, LinkBox, LinkOverlay } from "@chakra-ui/react";
+import NextImage from "next/image";
 
-const _HeroSocialIcon: React.FC<{ label: string; color: string; href: string }> = ({
-  label,
-  color,
-  href,
-  children,
-}) => (
-  <Tooltip label={label}>
-    <Link href={href} isExternal w={6} color={color}>
-      {children}
-    </Link>
-  </Tooltip>
-);
+import CheckOutMyLynk from "../../../public/img/check out my lynk.svg";
 
 export const Socials = () => {
   return (
-    <Stack direction={{ base: "row", sm: "column" }}>
-      <HStack spacing={{ base: 5, sm: 8 }}>
-        <_HeroSocialIcon label={"GitLab: portfolio work 💼"} color={"orange.600"} href={"https://gitlab.com/chroline"}>
-          <RiGitlabFill size={"100%"} />
-        </_HeroSocialIcon>
-        <_HeroSocialIcon
-          label={"GitHub: open source projects 👥"}
-          color={"gray.900"}
-          href={"https://github.com/chroline"}
-        >
-          <RiGithubFill size={"100%"} />
-        </_HeroSocialIcon>
-        <_HeroSocialIcon
-          label={"Instagram: my picturesque life 📸"}
-          color={"rose.600"}
-          href={"https://instagram.com/colegawin"}
-        >
-          <RiInstagramFill size={"100%"} />
-        </_HeroSocialIcon>
-        <_HeroSocialIcon
-          label={"Linkedin: my business life 🧑‍💼"}
-          color={"lightBlue.600"}
-          href={"https://linkedin.com/in/colegawin"}
-        >
-          <RiLinkedinBoxFill size={"100%"} />
-        </_HeroSocialIcon>
-        <_HeroSocialIcon
-          label={"Twitter: my marketable life 🤩"}
-          color={"lightBlue.500"}
-          href={"https://twitter.com/colegawin_"}
-        >
-          <RiTwitterFill size={"100%"} />
-        </_HeroSocialIcon>
-        <_HeroSocialIcon label={"Buy me a... cookie!"} color={"yellow.500"} href={"https://buymeacoffee.com/colegawin"}>
-          <SiBuymeacoffee size={"100%"} />
-        </_HeroSocialIcon>
-        <_HeroSocialIcon label={"Send me an email!"} color={"teal.600"} href={"mailto:hi@colegaw.in"}>
-          <RiAtFill size={"100%"} />
-        </_HeroSocialIcon>
-      </HStack>
-    </Stack>
+    <LinkBox>
+      <LinkOverlay href={"https://lynk.colegaw.in"} isExternal>
+        <Box w={72}>
+          <NextImage src={CheckOutMyLynk} alt={"check out my lynk"} />
+        </Box>
+      </LinkOverlay>
+    </LinkBox>
   );
 };
