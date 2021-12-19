76b3165680f364173dcb37dc03c61feeb9549411
Cole Gawin <colegawin@gmail.com>
2021-12-19 17:18:37 -0600

    🔧 resolve delayed rendering issue

--- src/components/navbar/Links.tsx
+++ src/components/navbar/Links.tsx
@@ -1,87 +1,27 @@
 import React from "react";
 
-import {
-  HStack,
-  IconButton,
-  Link,
-  LinkBox,
-  LinkOverlay,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  useBreakpointValue,
-} from "@chakra-ui/react";
+import { HStack, IconButton, Link, LinkBox, LinkOverlay, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { HiExternalLink, HiOutlineMenu } from "react-icons/hi";
 
-const _LGNavbarLinks = () => (
-  <HStack spacing={6} fontSize={"lg"} fontWeight={"medium"} color={"gray.700"}>
-    <Link href={"#"}>About me</Link>
-    <Link href={"https://by.colegaw.in/brandgineering"} isExternal>
-      Brand-gineering
-    </Link>
-    <Link href={"https://gitlab.com/chroline"} isExternal sx={{ display: "flex", alignItems: "center" }}>
-      Professional work
-      <span style={{ marginLeft: "5px" }}>
-        <HiExternalLink />
-      </span>
-    </Link>
-  </HStack>
-);
+function _LGNavbarLinks() {
+  return (
+    <HStack spacing={6} fontSize={"lg"} fontWeight={"medium"} color={"gray.700"}>
+      <Link href={"#"}>About me</Link>
+      <Link href={"https://by.colegaw.in/brandgineering"} isExternal>
+        Brand-gineering
+      </Link>
+      <Link href={"https://gitlab.com/chroline"} isExternal sx={{ display: "flex", alignItems: "center" }}>
+        Professional work
+        <span style={{ marginLeft: "5px" }}>
+          <HiExternalLink />
+        </span>
+      </Link>
+    </HStack>
+  );
+}
 
-const _SMNavbarLinks = () => (
-  <MenuList>
-    <LinkBox>
-      <MenuItem>
-        <LinkOverlay>About me</LinkOverlay>
-      </MenuItem>
-    </LinkBox>
-    <LinkBox>
-      <MenuItem>
-        <LinkOverlay href={"https://by.colegaw.in/brandgineering"} isExternal>
-          Brand-gineering
-        </LinkOverlay>
-      </MenuItem>
-    </LinkBox>
-    <LinkBox>
-      <MenuItem>
-        <LinkOverlay href={"https://gitlab.com/chroline"} isExternal sx={{ display: "flex", alignItems: "center" }}>
-          Professional work
-          <span style={{ marginLeft: "5px" }}>
-            <HiExternalLink />
-          </span>
-        </LinkOverlay>
-      </MenuItem>
-    </LinkBox>
-    <LinkBox>
-      <MenuItem display={{ base: "unset", sm: "none" }}>
-        <LinkOverlay href={"https://projects.colegaw.in"} isExternal sx={{ display: "flex", alignItems: "center" }}>
-          Portfolio
-          <span style={{ marginLeft: "5px" }}>
-            <HiExternalLink />
-          </span>
-        </LinkOverlay>
-      </MenuItem>
-    </LinkBox>
-    <LinkBox>
-      <MenuItem>
-        <LinkOverlay href={"https://blog.colegaw.in"} isExternal sx={{ display: "flex", alignItems: "center" }}>
-          Read my blog
-          <span style={{ marginLeft: "5px" }}>
-            <HiExternalLink />
-          </span>
-        </LinkOverlay>
-      </MenuItem>
-    </LinkBox>
-  </MenuList>
-);
-
-export const NavbarLinks = () => {
-  const isLG = useBreakpointValue({ base: false, lg: true }, "base");
-
-  return isLG ? (
-    <_LGNavbarLinks />
-  ) : (
+function _SMNavbarLinks() {
+  return (
     <Menu>
       <MenuButton
         as={IconButton}
@@ -90,7 +30,55 @@ export const NavbarLinks = () => {
         variant="ghost"
         aria-label={"menu"}
       />
-      <_SMNavbarLinks />
+      <MenuList>
+        <LinkBox>
+          <MenuItem>
+            <LinkOverlay>About me</LinkOverlay>
+          </MenuItem>
+        </LinkBox>
+        <LinkBox>
+          <MenuItem>
+            <LinkOverlay href={"https://by.colegaw.in/brandgineering"} isExternal>
+              Brand-gineering
+            </LinkOverlay>
+          </MenuItem>
+        </LinkBox>
+        <LinkBox>
+          <MenuItem>
+            <LinkOverlay href={"https://gitlab.com/chroline"} isExternal sx={{ display: "flex", alignItems: "center" }}>
+              Professional work
+              <span style={{ marginLeft: "5px" }}>
+                <HiExternalLink />
+              </span>
+            </LinkOverlay>
+          </MenuItem>
+        </LinkBox>
+        <LinkBox>
+          <MenuItem display={{ base: "unset", sm: "none" }}>
+            <LinkOverlay href={"https://projects.colegaw.in"} isExternal sx={{ display: "flex", alignItems: "center" }}>
+              Portfolio
+              <span style={{ marginLeft: "5px" }}>
+                <HiExternalLink />
+              </span>
+            </LinkOverlay>
+          </MenuItem>
+        </LinkBox>
+        <LinkBox>
+          <MenuItem>
+            <LinkOverlay href={"https://blog.colegaw.in"} isExternal sx={{ display: "flex", alignItems: "center" }}>
+              Read my blog
+              <span style={{ marginLeft: "5px" }}>
+                <HiExternalLink />
+              </span>
+            </LinkOverlay>
+          </MenuItem>
+        </LinkBox>
+      </MenuList>
     </Menu>
   );
+}
+
+export const NavbarLinks = {
+  LG: _LGNavbarLinks,
+  SM: _SMNavbarLinks,
 };
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,19 +2,19 @@ import React from "react";
 
 import { Flex } from "@chakra-ui/layout";
 import { Box, useBreakpointValue } from "@chakra-ui/react";
-import { useWindowScroll } from "react-use";
+import { useAsync, useWindowScroll } from "react-use";
 
 import { NavbarButtons } from "~/components/navbar/Buttons";
 import { NavbarLinks } from "~/components/navbar/Links";
 import { NameLogo } from "~/components/shared/NameLogo";
 
-export const Navbar = () => {
-  const isLG = useBreakpointValue({ base: false, lg: true }, "base");
+const _Wrapper: React.FC<{ display: Record<string, "flex" | "none"> }> = ({ children, display }) => {
   const { y: scroll } = useWindowScroll();
 
   return (
     <Flex
       as={"nav"}
+      display={display}
       w={"full"}
       h={20}
       align={"center"}
@@ -26,23 +26,59 @@ export const Navbar = () => {
       sx={{ background: "rgba(255, 255, 255, 0.9)", backdropFilter: "blur(10px)" }}
     >
       <Flex maxW={"7xl"} w={"full"} align={"center"} justify={"space-between"}>
-        {!isLG && (
-          <Box>
-            <NavbarLinks />
-          </Box>
-        )}
-        <Box
-          position={{ base: "absolute", lg: "unset" }}
-          left={{ base: "50%", lg: 0 }}
-          transform={{ base: "translateX(-50%)", lg: "none" }}
-        >
-          <NameLogo size={10} />
-        </Box>
-        {isLG && <NavbarLinks />}
-        <Box display={{ base: "none", sm: "unset" }}>
-          <NavbarButtons />
-        </Box>
+        {children}
       </Flex>
     </Flex>
+  );
+};
+
+function _SMNavbar() {
+  return (
+    <_Wrapper display={{ base: "flex", lg: "none" }}>
+      <NavbarLinks.SM />
+      <Box
+        position={{ base: "absolute", lg: "unset" }}
+        left={{ base: "50%", lg: 0 }}
+        transform={{ base: "translateX(-50%)", lg: "none" }}
+      >
+        <NameLogo size={10} />
+      </Box>
+      <Box display={{ base: "none", sm: "unset" }}>
+        <NavbarButtons />
+      </Box>
+    </_Wrapper>
+  );
+}
+
+function _LGNavbar() {
+  return (
+    <_Wrapper display={{ base: "none", md: "flex" }}>
+      <Box
+        position={{ base: "absolute", lg: "unset" }}
+        left={{ base: "50%", lg: 0 }}
+        transform={{ base: "translateX(-50%)", lg: "none" }}
+      >
+        <NameLogo size={10} />
+      </Box>
+      <NavbarLinks.LG />
+      <Box display={{ base: "none", sm: "unset" }}>
+        <NavbarButtons />
+      </Box>
+    </_Wrapper>
+  );
+}
+
+export const Navbar = () => {
+  const isLG = useBreakpointValue({ base: false, lg: true });
+
+  const { loading } = useAsync(async () => {});
+
+  return loading ? (
+    <>
+      <_LGNavbar />
+      <_SMNavbar />
+    </>
+  ) : (
+    <>{isLG ? <_LGNavbar /> : <_SMNavbar />}</>
   );
 };
